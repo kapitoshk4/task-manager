076985f414273fb3ddfe37b4b0e0e016965ec65c
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-04-15 21:34:51 +0300

    Add template_name to TaskListView

--- tasks/views.py
+++ tasks/views.py
@@ -9,4 +9,4 @@ from tasks.models import Task
 class TaskListView(LoginRequiredMixin, generic.ListView):
     model = Task
     context_object_name = "task_list"
-    # template_name = "tasks/tasks_list.html"
+    template_name = "tasks/task_list.html"
