76bbfc649da905a3b855c6a8d8b6ecdfa8d01935
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-04-18 14:57:08 +0300

    Add login url

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from tasks.views import index, TaskListView
+from tasks.views import index, TaskListView, UserLoginView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("login/", UserLoginView.as_view(), name="login"),
     path("tasks/",
          TaskListView.as_view(),
          name="task-list"
