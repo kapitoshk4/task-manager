e46a9526cdf7d89748bb16586bfd5dcfc13b953c
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-04-18 11:49:40 +0300

    Create admin.py & add admin models

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from tasks.models import (
+    Task,
+    Position,
+    TaskType,
+    Worker,
+    Project
+)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["name", "description", "deadline", "status", ]
+    list_filter = ["deadline", "status"]
+    search_fields = ["name", ]
+
+
+@admin.register(Worker)
+class Worker(UserAdmin):
+    list_display = UserAdmin.list_display + ("position", )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "position",
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(Project)
+class Project(admin.ModelAdmin):
+    list_display = ["name", "description", "creator", ]
+    search_fields = ["name", ]
+
+
+admin.site.register(Position)
+admin.site.register(TaskType)
