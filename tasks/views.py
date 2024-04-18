3442ac263bd223f4d057e01e140f069fc59109ec
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-04-18 17:03:36 +0300

    Add ProjectListView & assignees count

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
+from django.db.models import Count
 from django.shortcuts import render
 from django.views import generic
 from django.views.generic import ListView
 
+from tasks import models
 from tasks.forms import LoginForm
 from tasks.models import Task, Project
 
@@ -28,3 +30,14 @@ class TaskListView(LoginRequiredMixin, generic.ListView):
     model = Task
     context_object_name = "task_list"
     template_name = "tasks/task_list.html"
+
+
+class ProjectListView(LoginRequiredMixin, generic.ListView):
+    model = Project
+    context_object_name = "project_list"
+    template_name = "tasks/project_list.html"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.annotate(num_assignees=Count("assignees"))
+        return queryset
