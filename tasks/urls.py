993e5166162f580912c7e55c4a8a12c7523bc930
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-04-18 11:48:12 +0300

    Add index url

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from tasks.views import TaskListView
+from tasks.views import index, TaskListView
 
 urlpatterns = [
+    path("", index, name="index"),
     path("tasks/",
          TaskListView.as_view(),
          name="task-list"
