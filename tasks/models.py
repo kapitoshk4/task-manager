effdcc9c219dce24b65d430ef5204889aff87b36
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-04-29 19:40:06 +0300

    Change field in Project model to title

--- tasks/models.py
+++ tasks/models.py
@@ -65,14 +65,14 @@ class Worker(AbstractUser):
 
 
 class Project(models.Model):
-    name = models.CharField(max_length=63)
+    title = models.CharField(max_length=63)
     description = models.TextField()
     creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     assignees = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="projects")
     invitation_code = models.UUIDField(editable=False, unique=True, null=True, blank=True)
 
     class Meta:
-        ordering = ("name", )
+        ordering = ("title", )
 
     def get_absolute_url(self):
         return reverse("tasks:project-detail", kwargs={"pk": self.pk})
