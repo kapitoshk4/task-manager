15ad6dc23548f8fea381762e97f47bda5afd9b0b
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-05-15 17:06:36 +0300

    Add django_toolbar

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "tasks",
+    "debug_toolbar"
 ]
 
 TASKS_APP_NAMESPACE = "tasks"
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "task_manager.urls"
@@ -136,3 +138,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR / "media",)
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+INTERNAL_IPS = {
+    "127.0.0.1",
+}
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -26,3 +26,6 @@ urlpatterns = [
     settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT
 )
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += path("__debug__/", include(debug_toolbar.urls)),
