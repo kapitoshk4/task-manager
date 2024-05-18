9b8b191fdd271442b79788a0f274237f3c443dfe
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-05-18 17:47:07 +0300

    Add re_path to urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path("blog/", include("blog.urls"))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
+from django.views.static import serve
+
 from task_manager import settings
 
 urlpatterns = [
@@ -26,6 +28,12 @@ urlpatterns = [
     settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT
 )
+
+urlpatterns += [
+    re_path(r"^media/(?P<path>.*)$", serve, {
+        "document_root": settings.MEDIA_ROOT
+    })
+]
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += path("__debug__/", include(debug_toolbar.urls)),
